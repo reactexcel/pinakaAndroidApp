c4e6b9c26a958c7d2baaca835711e4d7a90cc1c5
reactexcel <arun.etech2011@gmail.com>
2018-11-29 13:06:48 +0530

    asdas

---
+++ android/app/src/main/java/com/pinaka/MainApplication.java
@@ -0,0 +1,67 @@
+package com.pinaka;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import cl.json.RNSharePackage;
+import cl.json.ShareApplication;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ShareApplication, ReactApplication {
+
+
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+    protected static CallbackManager getCallbackManager() {
+      return mCallbackManager;
+    }
+
+  @Override
+     public String getFileProviderAuthority() {
+            return "com.pinaka.fileprovider";
+     }
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
+            new MapsPackage(),
+            new VectorIconsPackage(),
+            new RNSharePackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    FacebookSdk.sdkInitialize(getApplicationContext());
+  // If you want to use AppEventsLogger to log events.
+  AppEventsLogger.activateApp(this);
+  }
+}
